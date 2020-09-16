d584da841c959e4d8c49efb1323985742d4d7eda
Nakarin <nakarin>
2020-09-16 15:01:03 +0700

    Update quiz2.js

--- JS/quiz2.js
+++ JS/quiz2.js
@@ -9,5 +9,5 @@ for (let i = 1; i <= 20; i++) {
 let result1 = [-1, 2, 3, 56, 6, -93, 0, 4].map(el => el*2)
 console.log(result1)
 
-let result2 = [10, 13 ,44, 55, 89, 0, 12, 1].filter(el => el/2 == 0)
+let result2 = [10, 13 ,44, 55, 89, 0, 12, 1].filter(el => el%2 === 0)
 console.log(result2)
