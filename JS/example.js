4acfdca628d2500359aa7cbfbf5993b964d0ac41
Nakarin <nakarin>
2020-09-16 14:07:59 +0700

    add
    เพิ่มฟังก์ชัน

---
+++ JS/example.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+
+const PI = 3.14
+console.log("PI:",PI)
+
+
---
+++ JS/quiz1.js
@@ -0,0 +1,8 @@
+let me = {
+    firstname: 'Nakarin' ,
+    lastname: 'Keawto' ,
+    age: '22' ,
+    hobbies: 'animetion' ,
+    tel: '079963101'
+}
+console.log(me)
